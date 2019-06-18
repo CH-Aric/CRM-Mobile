861e8bdb5b9ce4a371d2fbc9abaa261833d190da
Aric <aric@coolheatcomfort.ca>
2019-06-18 11:24:02 -0400

    Revert "stuff"
    This reverts commit e91809f0cec997d6c9a42da8aabdc5785af6508a.

--- MainCRMV2/MainCRMV2/Pages/Inventory/Items_Page.xaml.cs
+++ MainCRMV2/MainCRMV2/Pages/Inventory/Items_Page.xaml.cs
@@ -53,41 +53,14 @@ namespace MainCRMV2.Pages.Inventory
         {
             string[] input = FormatFunctions.SplitToPairs(result);
             dict = FormatFunctions.createValuePairs(input);
-            if (dict.Count > 0)
-            {
-                PurgeCells();
-                for (int i = 0; i < dict["Description"].Count; i++)
-                {
-                    DataViewCell c = new DataViewCell(int.Parse(dict["IDKey"][i]));
-                    cells.Add(c);
-                    Label e = new Label() { HorizontalOptions = LayoutOptions.FillAndExpand };
-                    e.Text = dict["Description"][i];
-                    Entry e2 = new Entry() { HorizontalOptions = LayoutOptions.FillAndExpand };
-                    e2.Placeholder = dict["Price"][i];
-                    Label e3 = new Label() { HorizontalOptions = LayoutOptions.FillAndExpand };
-                    e3.Text = dict["Total"][i];
-                    StackLayout s = new StackLayout() { Orientation = StackOrientation.Horizontal };
-                    c.View = s;
-                    s.Children.Add(e);
-                    s.Children.Add(e3);
-                    s.Children.Add(e2);
-                    TSection.Add(c);
-                }
-            }
+
         }
         public void onClickedViewAudit(object sender, EventArgs e)
         {
-            string sql = "SELECT SUM(audits.Quantity) total,items.Description,items.Price FROM audits LEFT JOIN items ON items.IDKey=stock.ItemID GROUP BY items.IDKey;";
+            string sql = "SELECT SUM(audits.Quantity) total,items.Description,items.Price FROM audits LEFT JOIN items ON items.IDKey=stock.ItemID GROUP BY audits.IDKey;";
             TaskCallback call = populateTableWithAudit;
             DatabaseFunctions.SendToPhp(false,sql,call);
         }
-        public void PurgeCells()
-        {
-            foreach (DataViewCell item in cells)
-            {
-                this.TSection.Remove(item);
-            }
-            cells = new List<DataViewCell>();
-        }
+
     }
 }
